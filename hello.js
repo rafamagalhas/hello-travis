74372a0f8bc8f7203e9dabdc0b78954421ed02ac
rafamagalhas <magalhas.rafa@gmail.com>
2018-08-14 17:32:12 -0300

    Fix hello syntax

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-const http = require('http')
+var http = require('http');
 
 //create a server
-http.createServer( (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/json'})
-  res.end(JSON.stringify({'message': 'Hello Travis!'}))
-}).listen(1337, '127.0.0.1')
-console.log('Server runing at http://127.0.0.1:1337/')
+http.createServer( function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/json'});
+  res.end(JSON.stringify({'message': 'Hello Travis!'}));
+}).listen(1337, '127.0.0.1');
+console.log('Server runing at http://127.0.0.1:1337/');
