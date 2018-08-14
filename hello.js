c76ef8f7e658fa40681c776d35aa4aa9bffa81eb
Rafael Magalhaes <rafael@orgsystem.com.br>
2018-08-14 16:39:04 -0300

    Added http server

---
+++ hello.js
@@ -0,0 +1,7 @@
+import * as http from 'http'
+
+http.createServer( (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/json'})
+  res.end(JSON.stringify({'message': 'Hello Travis!'}))
+}).listen(1337, '127.0.0.1')
+console.log('Server runing at http://127.0.0.1:1337/')
